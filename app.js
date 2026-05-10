ca8bb4600d611de0402af95cf08660555e5769a4
Sana Ullah Siddiqui <sanaullah.siddiqui0317@gmail.com>
2026-05-10 23:37:33 +0500

    some problems solved

---
+++ app.js
@@ -0,0 +1,24 @@
+if (true) {
+    var a = 1;
+    let b = 2;
+    console.log(b)
+
+}
+
+console.log(a)
+// console.log(b)
+
+// var keyword is a functions block scope and used with any keyword then working global scope 
+
+
+function check() {
+    var g = 12;
+    // console.log(g)
+}
+check()
+
+var g = 15
+console.log(g)
+
+
+console.log(+'5')
---
+++ operatorsProblem.js
@@ -0,0 +1,43 @@
+// Q1: Create two variables a and b and print:
+
+// addition
+// subtraction
+// multiplication
+// division
+
+const a = 5
+const b = 20
+console.log(a+b)
+console.log(a-b)
+console.log(a/b)
+console.log(a*b)
+
+// Q2: Find the remainder when 25 is divided by 4.
+
+const reminder = 25%4
+console.log(reminder)
+// Ans:1
+
+// Q3: Calculate 2 power 5 using operator.
+const power = 2*5
+console.log(power)
+
+// Q 4:Take a number and check if it is even or odd using %.
+const check = 8
+if(check%3 === 0){
+    console.log("odd")
+}else{
+    console.log("even")
+}
+
+// Q5: Convert temperature from Celsius to Fahrenheit using operators.
+const temperature = (34*9/5)+32
+console.log(temperature)
+
+// Q6: Create a variable count = 10.
+// Add 5
+// Subtract 2
+// Multiply by 3
+
+const count = 10;
+console.log()
